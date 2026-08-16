5b25fe17d53ca3160e8b51f385f22297100dd256
tamrayson2014-ops <tamrayson2014@gmail.com>
2026-08-16 18:58:51 +0800

    Update game.js

--- game.js
+++ game.js
@@ -3,17 +3,17 @@ document.addEventListener('keydown', handleKeyPress);
 let position = 0;
 
 function handleKeyPress(e) {
-  if (e.code === 'ArrowLeft') {
-    position = position - 10;
-  }
-  if (e.code === 'ArrowRight') {
-    position = position + 10;
-  }
-  if (position < 0) {
-    position = 0;
-  }
-  refresh();
+	if (e.code === 'ArrowLeft') {
+		position = position - 10;
+	}
+	if (e.code === 'ArrowRight') {
+		position = position + 10;
+	}
+	if (position < 0) {
+		position = 0;
+	}
+	refresh();
 }
 function refresh() {
-ball.style.left = position + 'px';
+	ball.style.left = position + 'px';
 }
